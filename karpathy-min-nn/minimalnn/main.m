22dca9523b5d65b9bda8e0ea251f1d4e55c56d18
Adam Wulf <adam.wulf@gmail.com>
2015-07-08 22:29:25 -0500

    reorg

---
+++ karpathy-min-nn/minimalnn/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  minimalnn
+//
+//  Created by Adam Wulf on 7/8/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
